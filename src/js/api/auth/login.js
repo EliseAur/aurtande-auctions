9942df113b5d0ddba552c61fc5ce7276cfc0e404
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-04-02 12:01:33 +0200

    Register and login functionality in progress

---
+++ src/js/api/auth/login.js
@@ -0,0 +1,29 @@
+import { API_AUCTION_URL } from "../constants.js";
+import * as storage from "../../storage/index.js";
+
+const action = "/auth/login";
+const method = "post";
+
+export async function login(profile) {
+  const loginURL = API_AUCTION_URL + action;
+  console.log("This is the login URL", loginURL);
+
+  const body = JSON.stringify(profile);
+  console.log(body);
+
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-type": "application/json",
+    },
+    method,
+    body,
+  });
+
+  const { accessToken, ...user } = await response.json();
+  storage.save("token", accessToken);
+  storage.save("profile", user);
+  storage.save("userName", user.name);
+
+  alert("You are now logged in");
+  window.location.href = "../../listings-member/index.html";
+}
---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import * as listeners from "./handlers/index.js";
+
+/**
+ * Sets up routing behavior and initializes event listeners based on the current URL path.
+ * This function ensures that the appropriate listeners and actions are executed for each page.
+ * If the current path does not match any predefined cases, no specific actions are taken.
+ *
+ * @returns {void}
+ *
+ * @example
+ * // Example usage:
+ * // This function is typically called once when the application starts to set up the initial state.
+ * setupRoutingAndListeners();
+ */
+function setupRoutingAndListeners() {
+  switch (location.pathname) {
+    case "/pages/account/login/":
+    case "/pages/account/login/index.html":
+    case "/aurtande-auctions/pages/account/login/":
+    case "/aurtande-auctions/pages/account/login/index.html":
+      listeners.setLoginFormListener();
+      break;
+    case "/pages/account/register/":
+    case "/pages/account/register/index.html":
+    case "/aurtande-auctions/pages/account/register/":
+    case "/aurtande-auctions/pages/account/register/index.html":
+      listeners.setRegisterFormListener();
+      break;
+    default:
+  }
+}
+
+setupRoutingAndListeners();
