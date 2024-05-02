17f53be541c84784a5dfb48e64b2f9a721cd035c
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-05-02 13:59:15 +0200

    update avatar possible fix

--- src/js/handlers/updateProfileImg.js
+++ src/js/handlers/updateProfileImg.js
@@ -11,7 +11,7 @@ import * as profileMethods from "../api/account/index.js";
  */
 export async function setUpdateProfileFormListener() {
   const form = document.querySelector("#editProfile");
-  const button = document.querySelector("button");
+  const button = document.querySelector("form button");
 
   if (form && button) {
     //disable the form with the button before it is uploaded
